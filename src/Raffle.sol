986f5ac5c195fdc35f71dd85eb97f0b4169115ec
legendarycode3 <ifeanyimaduchemm@gmail.com>
2026-04-06 17:23:02 +0100

    feat: add ETH entry fee check for raffle

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,3 +1,28 @@
+// Layout of Contract:
+// license
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// internal & private view & pure functions
+// external & public view & pure functions
+
+
+
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 
 pragma solidity ^0.8.19;
@@ -12,6 +37,9 @@ pragma solidity ^0.8.19;
 
 
 contract Raffle {
+    
+    /** Errors */
+    error Raffle__SendMoreEThToEnterRaffle();
 
     uint256 private immutable i_entranceFee;
 
@@ -19,8 +47,11 @@ contract Raffle {
         i_entranceFee = entranceFee;
     }
 
-    function enterRaffle() public {
-        
+    function enterRaffle() public payable {
+        // require(msg.value >= i_entranceFee, "Not enough ETH sent!");
+        if(msg.value >= i_entranceFee) {
+            revert Raffle__SendMoreEThToEnterRaffle();
+        }
     }
 
     function pickWinner() public {
