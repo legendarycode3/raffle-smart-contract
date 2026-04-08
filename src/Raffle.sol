8a8f1c320daca34a0876e8ecea7550a50f4fe6fd
legendarycode3 <ifeanyimaduchemm@gmail.com>
2026-04-08 11:56:01 +0100

    feat(raffle): add block.timestamp check to pickWinner function

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -44,18 +44,23 @@ contract Raffle {
 
 
     uint256 private immutable i_entranceFee;
+    uint256 private immutable i_interval;
     address payable[] private s_players;
+    // @dev The duration of the lottery in seconds.
+    uint256 private s_lastTimeStamp;
 
 
     event RaffleEntered(address indexed player);
 
     
-    constructor(uint256 entranceFee) {
+    constructor(uint256 entranceFee, uint256 intervl) {
         i_entranceFee = entranceFee;
+        i_interval = interval;
+        s_lastTimeStamp = block.timestamp;
     }
 
 
-    function enterRaffle() public payable {
+    function enterRaffle() external payable {
         // require(msg.value >= i_entranceFee, "Not enough ETH sent!");
         if(msg.value >= i_entranceFee) {
             revert Raffle__SendMoreEThToEnterRaffle();
@@ -66,8 +71,15 @@ contract Raffle {
     }
 
 
-    function pickWinner() public {
-
+    // 1. Get a random num.
+    // 2. Use that random num, to pick a player(s_players).
+    // 3. Be able to do this all time , automatically.
+    function pickWinner() external {
+        
+        //Check to see if enough time has passed
+        if ((block.timestamp - s_lastTimeStamp) > i_interval) {
+            revert();
+        }
     }
 
 
