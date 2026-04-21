5a948d219df66317f4fcb55bd0f2bad129e6de39
legendarycode3 <ifeanyimaduchemm@gmail.com>
2026-04-21 17:48:56 +0100

    chore(deploy): deploy raffle contract

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.19;
 
 import {Script} from "forge-std/Script.sol";
 import {Raffle} from "../src/Raffle.sol";
-import {HelperConfig} from "../script/HelperConfig.s.sol";
+import {HelperConfig} from "script/HelperConfig.s.sol";
 
 contract DeployRaffle is Script {
 
@@ -12,7 +12,27 @@ contract DeployRaffle is Script {
 
     }
 
-    // function deployContract() public returns(Raffle, HelperConfig) {
+    function deployContract() public returns(Raffle, HelperConfig) {
+        HelperConfig helperConfig = new HelperConfig();
 
-    // }
+        /// @notice If we are on Local Network => Deploy on Mocks , getLocalConfig
+        /// @notice And if we are on Sepolia Network => getSepoliaConfig()
+        HelperConfig.NetworkConfig memory config = helperConfig.getConfig(); 
+
+        vm.startBroadcast();
+
+        Raffle raffle = new Raffle(
+            config.entranceFee,
+            config.interval,
+            config.vrfCoordinator,
+            config.gasLane,
+            config.subscriptionId,
+            config.callBackGasLimit
+        );
+
+        vm.stopBroadcast();
+
+        return (raffle, helperConfig);
+    }
+
 }
--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -3,8 +3,15 @@
 pragma solidity ^0.8.19;
 
 import {Script} from "forge-std/Script.sol";
+import {VRFCoordinatorV2_5Mock} from "@chainlink/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol";
 
 abstract contract CodeConstants {
+     /**VRF mock values */
+   uint96 public MOCK_BASE_FEE = 0.25 ether;
+   uint96 public MOCK_GAS_PRICE_LINK = 1e9;
+   // LINK  / ETH price
+   int256 public MOCK_WEI_PER_UNIT_LINK =  4e15;
+
    uint256 public constant ETH_SEPOLIA_CHAIN_ID = 11155111;
    uint256 public constant LOCAL_CHAIN_ID = 31337;
 }
@@ -40,19 +47,27 @@ contract HelperConfig is CodeConstants , Script {
         networkConfigs[ETH_SEPOLIA_CHAIN_ID] = getSepoliaEthConfig();
     }
 
+
     /// @notice Function for selecting a specific network to deploy on depending on the chainId provided
     function getConfigByChainId(uint256 chainId) public returns(NetworkConfig memory){
         if(networkConfigs[chainId].vrfCoordinator != address(0)) {
             return networkConfigs[chainId];
         }
         else if(chainId == LOCAL_CHAIN_ID){
-            // getOrCreateAnvilEthConfig()
+            return getOrCreateAnvilEthConfig();
 
         }
         else {
             revert HelperConfig__InvalidChainId();
         }
     }
+
+
+
+    function getConfig() public returns(NetworkConfig memory){
+        return getConfigByChainId(block.chainid);
+    }
+
 
     /// @notice Function for deploying on a sepoliaTestnet.
     function getSepoliaEthConfig() public pure returns(NetworkConfig memory){
@@ -66,11 +81,29 @@ contract HelperConfig is CodeConstants , Script {
         });
     }
 
+
     function getOrCreateAnvilEthConfig() public returns(NetworkConfig memory) {
         /// @notice Check if we set an active network config.
         if(localNetworkConfig.vrfCoordinator != address(0)){
             return localNetworkConfig;
         }
+
+        /// @notice Deploying with mocks on  Anvil local chain 
+        vm.startBroadcast();
+
+        VRFCoordinatorV2_5Mock vrfCoordinatorMock = new VRFCoordinatorV2_5Mock(MOCK_BASE_FEE, MOCK_GAS_PRICE_LINK, MOCK_WEI_PER_UNIT_LINK);
+
+        vm.stopBroadcast();
+
+        localNetworkConfig = NetworkConfig({
+            entranceFee: 0.01 ether,  //10000000000000000  or 1e16 (all same)
+            interval: 30, //30 seconds
+            vrfCoordinator: address(vrfCoordinatorMock),
+            gasLane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
+            callBackGasLimit:  500000, // 500,000 gas (I Hardcoded It)
+            subscriptionId: 0
+        }); 
+        return localNetworkConfig;
     }
 
 
